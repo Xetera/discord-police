f9f4fc6690e8c1a36682b44455f042468fd07d6c
Xetera <mythologyali@gmail.com>
2019-07-04 19:15:25 -0700

    typo fix

--- src/FFI/FS.purs
+++ src/FFI/FS.purs
@@ -11,7 +11,7 @@ import Node.Path (FilePath)
 
 type FileInfo_ =
   { name :: FilePath
-  , fileType :: Strin
+  , fileType :: String
   }
 
 foreign import _readDir :: FilePath -> EffectFnAff (Array FileInfo_)
